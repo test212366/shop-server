83c2ba9281f2f95e37ba7299efe30046f6982581
Nikita.Zmanovskiy <72348175+test212366@users.noreply.github.com>
2023-10-23 21:58:57 +0300

    Add files via upload

---
+++ routes/brandRouter.js
@@ -0,0 +1,7 @@
+const Router = require('express')
+const router = new Router()
+const BrandController = require('../controllers/brandController')
+router.post('/', BrandController.create)
+router.get('/', BrandController.getAll)
+
+module.exports = router
---
+++ routes/userRouter.js
@@ -0,0 +1,11 @@
+const Router = require('express')
+const userController = require('../controllers/userController')
+const router = new Router()
+const UserController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
+
+router.post('/registration', UserController.registration)
+router.post('/login', UserController.login)
+router.get('/auth', authMiddleware, userController.check)
+
+module.exports = router
